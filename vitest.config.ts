4e7f5c347680ebfaa55d65eeb8a89fd8355c1ca7
yanggmtl <80186321+yanggmtl@users.noreply.github.com>
2025-12-18 12:31:09 -0500

    Fix non exist c8 provider issue

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
@@ -11,7 +11,7 @@ export default defineConfig({
     setupFiles: './tests/setup.ts',
     css: true,
     coverage: {
-      provider: 'c8',
+      provider: 'v8', 
       reporter: ['text', 'json', 'html'],
       exclude: [
         'node_modules/',

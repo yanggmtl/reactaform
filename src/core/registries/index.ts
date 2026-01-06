203d695f6eef88298ee1750abf7d7acc22596af0
yanggmtl <80186321+yanggmtl@users.noreply.github.com>
2026-01-06 10:56:25 -0500

    Move validation related files to validation folder. Also remove registries folder and move rest files to core

--- src/core/registries/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./componentRegistry";
-export * from "./submissionHandlerRegistry";
-export * from "./validationHandlerRegistry";
-export * from "./baseRegistry";
-export * from "./pluginRegistry";
-
-export { default as componentRegistry } from "./componentRegistry";
-export { default as submissionRegistry } from "./submissionHandlerRegistry";
-export { default as validatorRegistry } from "./validationHandlerRegistry";
